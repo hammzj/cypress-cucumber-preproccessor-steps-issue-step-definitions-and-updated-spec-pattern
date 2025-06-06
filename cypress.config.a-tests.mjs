6c76aa5ad51f66b3a59f6d1c7f198e67dcdcf23e
Zachary Hamm <zjhamm304@gmail.com>
2025-06-06 10:38:14 -0400

    add:rename config files

--- cypress.config.a-tests.mjs
+++ cypress.config.a-tests.mjs
@@ -7,7 +7,7 @@ const mutatedConfig = baseConfig;
 mutatedConfig.e2e.specPattern = 'cypress/e2e/features/a-tests/**/*.feature';
 mutatedConfig.e2e.env.stepDefinitions = [
     "cypress/e2e/features/stepDefinitions/**/*.{js,mjs,cjs}",
-    "cypress/e2e/features/a-tests-tests/[filepart]/stepDefinitions/**/*.{js,mjs,cjs}"
+    "cypress/e2e/features/a-tests/[filepart]/stepDefinitions/**/*.{js,mjs,cjs}"
 ]
 
 // Export new config
--- cypress.config.b-tests.mjs
+++ cypress.config.b-tests.mjs
@@ -4,10 +4,10 @@ import baseConfig from './cypress.config.mjs';
 const mutatedConfig = baseConfig;
 
 // Edits
-mutatedConfig.e2e.specPattern = 'cypress/e2e/features/b-tests-tests/**/*.feature';
+mutatedConfig.e2e.specPattern = 'cypress/e2e/features/b-tests/**/*.feature';
 mutatedConfig.e2e.env.stepDefinitions = [
     "cypress/e2e/features/stepDefinitions/**/*.{js,mjs,cjs}",
-    "cypress/e2e/features/b-tests-tests/[filepart]/stepDefinitions/**/*.{js,mjs,cjs}"
+    "cypress/e2e/features/b-tests/[filepart]/stepDefinitions/**/*.{js,mjs,cjs}"
 ]
 
 // Export new config
